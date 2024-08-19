d9ba0bf7b0dd2c66aa428d5e3173fd662fc343ba
Symon R <symonr@videx.com>
2024-08-19 16:37:17 -0700

    [Data] Add strings.

---
+++ constants/Strings.ts
@@ -0,0 +1,34 @@
+
+// What's been going on in your life?
+export const PromptExamples = {
+    church: [
+        "This weekend, I was at church! The message was about..."
+    ],
+    devotionalLife: [
+        "I was in the Bible reading about...",
+        "There's this worship song that I've been loving called...",
+        "I've been praying pretty often about...",
+    ],
+    ministries: [
+        "I was serving with my church at...",
+    ],
+    missions: [
+        "I recently went on a missions trip in..."
+    ],
+};
+
+export const PromptQuestions = {
+    church: [
+        "Hey, want to check out my church this Sunday?",
+        "Do you have any background in faith?"
+    ],
+    spiritual: [
+        "Have you had any spiritual experiences lately?",
+        "What do you think about Jesus?",
+        "If you were in front of God right now, what one question would you ask him?",
+        "Do you have any questions about Christianity?"
+    ],
+    general: [
+        "What's been going in in your life?"
+    ],
+};
