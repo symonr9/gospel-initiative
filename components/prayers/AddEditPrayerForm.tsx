990132a86d54964d7a3a6231b6c8c8d332190b94
Symon R <symonr@videx.com>
2024-08-20 16:11:23 -0700

    [UI] Add forms.

---
+++ components/prayers/AddEditPrayerForm.tsx
@@ -0,0 +1,32 @@
+
+import React, { useState, useEffect } from 'react';
+
+import { connect } from 'react-redux';
+import { FlatList, View, ViewProps } from 'react-native';
+
+import Prayer from '@/models/prayer';
+import { Colors, useBackgroundThemeColor } from '@/constants/Colors';
+
+export type IAddEditPrayerForm = ViewProps & {
+    prayer: Prayer;
+};
+
+function AddEditPrayerForm({ style, prayer, ...otherProps }: IAddEditPrayerForm) {
+    const backgroundColor = useBackgroundThemeColor();
+
+    return (
+        <View style={[{ backgroundColor }, style]} {...otherProps}>
+
+        </View>
+    );
+}
+
+const mapStateToProps = (state: IAddEditPrayerForm) => ({
+    prayer: state.prayer,
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddEditPrayerForm);
