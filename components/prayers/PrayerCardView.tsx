5547676cda52b6bef08ff831648240d27facc8cd
Symon R <symonr@videx.com>
2024-08-22 13:12:02 -0700

    [UI] Progress on components.

--- components/prayers/PrayerCardView.tsx
+++ components/prayers/PrayerCardView.tsx
@@ -1,55 +1,23 @@
 import React from 'react';
 import { View, type ViewProps } from 'react-native';
-import { StyleSheet, Dimensions } from 'react-native';
-import { Image } from 'expo-image';
 
-import { flexStyles } from '@/styles/Styles';
-
-import { Colors, useBackgroundThemeColor } from '@/constants/Colors';
-import One from '@/models/one';
-import { ThemedText, ThemedTextType } from '../common/ThemedText';
-import { ThemedView } from '../common/ThemedView';
+import Prayer from '@/models/prayer';
+import { ThemedText } from '../common/ThemedText';
+import { SimpleCardView } from '../common/SimpleCardView';
+import { AppIcon } from '@/enums/enums';
 
 export type IPrayerCardView = ViewProps & {
-  one: One;
+  prayer: Prayer;
 };
 
-export function PrayerCardView({ one }: IPrayerCardView) {
-    const backgroundColor = useBackgroundThemeColor();
+export function PrayerCardView({ prayer }: IPrayerCardView) {
+  const detailsView = (
+    <ThemedText>Test</ThemedText>
+  );
 
   return (
-    <ThemedView style={styles.container}>
-      <View style={flexStyles.column}>
-        <Image source={one.icon} style={styles.icon} contentFit="contain" />
-        <ThemedText type={ThemedTextType.Title}>{one.name}</ThemedText>
-      </View>
-      <View style={flexStyles.column}>
-        <ThemedText>Test</ThemedText>
-      </View>
-    </ThemedView>
+    <SimpleCardView iconSrc={AppIcon.Man1}
+                    title={prayer.name}
+                    detailsView={detailsView} />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    flex: 1,
-    backgroundColor: '#F0E68C', // Khaki color
-    borderRadius: 4,
-    padding: 16,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.3,
-    shadowRadius: 6, // Shadow radius for a softer shadow
-    elevation: 4,
-  },
-  icon: {
-    width: 80,
-    height: 80,
-    marginBottom: 12, // Space between the icon and the name
-  },
-});
