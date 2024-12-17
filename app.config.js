1613e084608b98ddf1a5f4cbd80816ce3596e7fc
symonr9 <symonr12@gmail.com>
2024-12-16 21:37:38 -0800

    [Release] v1.0.2

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default ({ config }) => ({
     expo: {
         name: "gospel-initiative",
         slug: "gospel-initiative",
-        version: "1.0.1",
+        version: "1.0.2",
         orientation: "portrait",
         icon: "./assets/images/gospel-initiative/app-logo.png",
         scheme: "myapp",
